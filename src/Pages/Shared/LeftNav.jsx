b2593bbdedea7a03b111138c8073a2ab1bdc1e93
IJE2704 <mh1669101@gmail.com>
2023-10-04 20:59:30 +0600

    left nav

--- src/Pages/Shared/LeftNav.jsx
+++ src/Pages/Shared/LeftNav.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
     const [categories,setCategories] = useState([]);
     useEffect(()=>{
         fetch('http://localhost:5000/catagories')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setCategories(data))
         .catch(error => console.log(error))
     },[])
     return (
         <div>
-            <h1>Left nav</h1>
+            <h4>All Categories</h4>
+            <h5>National News</h5>
+            <div className=' '>
+                {
+                    categories.map(cat => <p  key={cat.id}>
+                        <Link className='text-decoration-none text-black'>{cat.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
