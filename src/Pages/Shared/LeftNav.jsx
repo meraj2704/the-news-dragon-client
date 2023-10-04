edbd5c81a6aa51ed70c7da7e8956c31d1dc29164
IJE2704 <mh1669101@gmail.com>
2023-10-04 20:42:27 +0600

    get data from backend

--- src/Pages/Shared/LeftNav.jsx
+++ src/Pages/Shared/LeftNav.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+    const [categories,setCategories] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/catagories')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    },[])
     return (
         <div>
             <h1>Left nav</h1>
