6e4bc96a84ee8c0f26cd6840942301b2c83f8770
IJE2704 <mh1669101@gmail.com>
2023-10-04 20:00:10 +0600

    right nav done

--- src/Pages/Shared/RightNav.jsx
+++ src/Pages/Shared/RightNav.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 import { FaBeer, FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 import Qzone from './Qzone';
+import backgroundImage from '../../assets/bg.png'
+const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize:'cover',
+    backgroundRepeat:'no-repeat',
+
+}
+
 
 const RightNav = () => {
     return (
@@ -11,7 +19,7 @@ const RightNav = () => {
                 <Button className='mb-2' variant="outline-secondary"><FaGoogle></FaGoogle> Login with Google</Button>
                 <Button variant="outline-secondary"><FaGithub></FaGithub> Login with GitHub</Button>
             </div>
-            <div>
+            <div className='mb-3'>
                 <h4>Find Us</h4>
                 <ListGroup>
                     <ListGroup.Item>
@@ -22,6 +30,11 @@ const RightNav = () => {
                 </ListGroup>
             </div>
             <Qzone></Qzone>
+            <div style={backgroundStyle} className='px-4 py-5 text-white mt-3'>
+                <h3 className='mb-3'>Create an <br /> Amazing <br /> Newspaper</h3>
+                <p className='mb-3'>Discover thousands of options easy to customize layouts one click to import demo and much more</p>
+                <Button variant="danger">Learn More</Button>
+            </div>
         </div>
     );
 };
