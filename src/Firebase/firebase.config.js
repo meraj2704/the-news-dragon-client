e25526478b5dc1d4a20037c5a37e2bcb3f72dbc2
IJE2704 <mh1669101@gmail.com>
2023-10-15 15:47:27 +0600

    protect the site

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// console.log('enviroment variable ', import.meta.env.VITE_PASS)
 const firebaseConfig = {
-  apiKey: "AIzaSyAeWrbq5rEycFDQJTkIB1CtAtDOh3KkjnA",
-  authDomain: "the-news-dragon-fa428.firebaseapp.com",
-  projectId: "the-news-dragon-fa428",
-  storageBucket: "the-news-dragon-fa428.appspot.com",
-  messagingSenderId: "13941097014",
-  appId: "1:13941097014:web:9e5e009ca95141d07f8e97"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
